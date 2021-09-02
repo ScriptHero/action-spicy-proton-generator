57072032cc8acced4142a2e60ba5808d97e2833c
Robin Daugherty <robin@robindaugherty.net>
2021-09-02 12:00:58 -0400

    Set an output

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,12 @@
 
 branch_name=$1
 
-ruby -rspicy-proton -rdigest/sha1 -e'
-  branch_name_hash = Digest::SHA1.hexdigest("${branch_name}").to_i(16)
+spicy_proton_name=$(ruby -rspicy-proton -rdigest/sha1 -e'
+  branch_name_hash = Digest::SHA1.hexdigest("'${branch_name}'").to_i(16)
   words            = Spicy::Proton.new
   adjective        = words.adjectives[branch_name_hash % words.adjectives.length]
   noun             = words.nouns[branch_name_hash % words.nouns.length]
   puts "#{adjective}-#{noun}"
-'
+')
+
+echo "::set-output name=result::${spicy_proton_name}"
