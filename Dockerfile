16f8f35cc58540f57de51107ec9b134e0140b4e6
Robin Daugherty <robin@robindaugherty.net>
2021-09-02 11:41:38 -0400

    Initial setup

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-FROM alpine:3.10
+FROM ruby:2.7-slim
 
 COPY LICENSE README.md /
+
+RUN gem install spicy-proton -v '2.1.9'
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh -l
 
-echo "hello $1"
+branch_name=$1
+
+ruby -rspicy-proton -rdigest/sha1 -e'
+  branch_name_hash = Digest::SHA1.hexdigest("${branch_name}").to_i(16)
+  words            = Spicy::Proton.new
+  adjective        = words.adjectives[branch_name_hash % words.adjectives.length]
+  noun             = words.nouns[branch_name_hash % words.nouns.length]
+  puts "#{adjective}-#{noun}"
+'
