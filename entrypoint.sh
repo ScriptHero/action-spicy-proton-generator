d0620e48fe2f1f239063e246ac27c459b396535e
Robin Daugherty <robin@robindaugherty.net>
2021-09-03 11:42:05 -0400

    Fix bug where spaces break everything

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-branch_name=$1
+export branch_name="$1"
 
 spicy_proton_name=$(ruby -rspicy-proton -rdigest/sha1 -e'
-  branch_name_hash = Digest::SHA1.hexdigest("'${branch_name}'").to_i(16)
+  branch_name_hash = Digest::SHA1.hexdigest(ENV["branch_name"]).to_i(16)
   words            = Spicy::Proton.new
   adjective        = words.adjectives[branch_name_hash % words.adjectives.length]
   noun             = words.nouns[branch_name_hash % words.nouns.length]
